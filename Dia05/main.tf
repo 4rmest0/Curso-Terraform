d87ecb110b75b3a513cb3fa8a9e1c97d4237cc84
4rmest0 <nicolasarmesto5.2005@gmail.com>
2026-04-22 13:37:48 +0200

    Día 05: Variables de entrada y salida en Terraform

---
+++ Dia05/main.tf
@@ -0,0 +1,57 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 4.8.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "tfstate-dia04"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    storage_account_name = "dia044020"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "tfstate"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "demo.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  }
+  required_version = ">= 1.9.0"
+}
+
+provider "azurerm" {
+  features {
+    
+  }
+  
+}
+variable "environment" {
+    type = string
+    description = "the env type"
+    default = "staging"
+  
+}
+
+locals {
+  common_tags ={
+    environment = "dev"
+    lob = "banking"
+    stage = "alfa"
+  }
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "pruebaterraform03"
+  resource_group_name      =  azurerm_resource_group.example.name
+  location                 =  azurerm_resource_group.example.location #dependencia implicita
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = local.common_tags.stage
+  }
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.example.name
+}   
